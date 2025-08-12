3531af90c494eb75632518c416fd1a43372dcec0
UrayNazwa <uraynazwa01@gmail.com>
2025-08-12 19:52:47 +0700

    Add files via upload

---
+++ layout.tsx
@@ -0,0 +1,30 @@
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+import "./globals.css";
+
+const poppins = Poppins({
+  weight: ['400', '500', '600', '700'],
+  subsets: ["latin"],
+  display: 'swap',
+  variable: "--font-poppins",
+});
+
+export const metadata: Metadata = {
+  title: "KemenHub",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${poppins.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
